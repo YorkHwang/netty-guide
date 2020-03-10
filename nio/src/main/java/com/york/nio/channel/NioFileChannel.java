1975eaa315207dde362d94f941bd77036d122279
walterhuang <walterhuang@kugou.com>
2020-03-10 08:15:42 +0800

    Channel&Buffer读写实战

---
+++ nio/src/main/java/com/york/nio/channel/NioFileChannel.java
@@ -0,0 +1,44 @@
+package com.york.nio.channel;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @Description:FileChannel写文件实例
+ * @Author: York.Hwang
+ * @Time: 2020/3/9 23:13
+ */
+public class NioFileChannel {
+
+    private static String CONTENT = "FileChannel的写文件实例";
+    private static String FILE_PATH = "/scala/a.txt";
+    private static String UTF8 = "UTF-8";
+
+
+    public static void main(String[] args) throws IOException {
+        //写文件
+        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+        byteBuffer.put(CONTENT.getBytes(UTF8));
+        byteBuffer.flip();
+
+        FileOutputStream fileOutputStream = new FileOutputStream(FILE_PATH);
+        FileChannel fileOutChannel = fileOutputStream.getChannel();
+        fileOutChannel.write(byteBuffer);
+        fileOutputStream.close();
+        System.out.println("写文件完成！");
+
+        //读文件
+        File file = new File(FILE_PATH);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        FileChannel fileInputChannel = fileInputStream.getChannel();
+
+        ByteBuffer fileInputBuffer = ByteBuffer.allocate((int)file.length());
+        fileInputChannel.read(fileInputBuffer);
+        fileInputStream.close();
+
+        System.out.println("读取文件的内容："+new String(fileInputBuffer.array()));
+
+    }
+
+}
---
+++ nio/src/main/java/com/york/nio/channel/NioFileCopy.java
@@ -0,0 +1,43 @@
+package com.york.nio.channel;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @Description:使用同一个Buffer拷贝文件
+ * @Author: York.Hwang
+ * @Time: 2020/3/9 23:36
+ */
+public class NioFileCopy {
+    private static final String SOURCE_FILE_PATH = "/scala/a.txt";
+    private static final String TARGET_FILE_PATH = "/scala/b.txt";
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(SOURCE_FILE_PATH);
+        FileChannel inputChannel = fileInputStream.getChannel();
+
+        FileOutputStream fileOutputStream = new FileOutputStream(TARGET_FILE_PATH);
+        FileChannel outputChannel = fileOutputStream.getChannel();
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+        while (true) {
+            //进行复位，继续读
+            byteBuffer.clear();
+            int read = inputChannel.read(byteBuffer);
+            //判断读完了
+            if (read == -1) {
+                break;
+            }
+            //进行反转，切换读写
+            byteBuffer.flip();
+            outputChannel.write(byteBuffer);
+        }
+        inputChannel.close();
+        outputChannel.close();
+
+        System.out.println("文件复制完成！");
+    }
+}
